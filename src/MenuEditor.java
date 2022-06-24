45cfd6d74b7b92a1d3247ea2767deef9e6f82827
kouheisatou <koheisato0420@gmail.com>
2022-06-24 14:53:03 +0900

    ADD : menu editor

--- src/MenuEditor.java
+++ src/MenuEditor.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class MenuEditor {
+    private JPanel root;
+    private JTextField textField1;
+    private JButton addUpdateButton;
+    private JComboBox comboBox1;
+    private JList list1;
 }
